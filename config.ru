b9cb7d9ba929559619820acb77190c06827ff5ed
Ettore Berardi <ettore.berardi@nature.com>
2016-03-13 19:47:35 +0000

    Use ENV vars to configure the app.

--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
 require 'Sky'
+require 'dotenv'
+Dotenv.load
+
 run Sky::App.new
--- lib/sky/bill_repository.rb
+++ lib/sky/bill_repository.rb
@@ -1,7 +1,7 @@
 module Sky
   class BillRepository
     include HTTParty
-    base_uri 'safe-plains-5453.herokuapp.com'
+    base_uri ENV['DATASOURCE_HOST']
 
     # We assume in real life we have some
     # sort of id for this.
